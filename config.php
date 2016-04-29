b8549826690f18c0173cb5dedc5d2f4ccf52d66d
ash <ash@microweber.com>
2016-04-29 18:23:00 +0300

    first

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+$config = array(
+	'name' => "Ink",
+	'link' => "https://github.com/net-shell/mw-ink",
+	'description' => "Printing materials editor module",
+	'author' => "ash",
+	'ui' => true,
+	'ui_admin' => true,
+	'position' => "4",
+	'version' => "0.0.1"
+);
