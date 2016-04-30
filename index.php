deb30c2ba2140659e5cac0b851436e6533339b3f
ash <ash@microweber.com>
2016-04-30 22:59:30 +0300

    p[=-

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
 	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 <![endif]-->
 <!--[if IE]><script type="text/javascript" src="<?php print $config['url_to_module']; ?>vendor/net-shell/mw-ink/js/excanvas.js"></script><![endif]-->
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>	
 <script type="text/javascript" src="<?php print $config['url_to_module']; ?>dist/ui.js"></script>
 
 <style type="text/css">
