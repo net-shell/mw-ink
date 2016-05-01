cdacfab23591774c19f99e7ddd473421d256d0b6
ash <ash@microweber.com>
2016-05-01 14:46:56 +0300

    ad

---
+++ admin.php
@@ -0,0 +1,8 @@
+<div class="mw-ui-field-holder">
+	<label class="mw-ui-label">
+		Setting
+		<small class="mw-help" data-help="If set this, the user will be page"> (?) </small>
+	</label>
+	<input name="mw-ink-setting" class="mw-ui-field mw_option_field"
+		type="text" value="<?php print get_option('mw-ink-setting', $params['id']); ?>">
+</div>
